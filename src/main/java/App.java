8bfd0761558217c9521e71310a6c4349540defbe
Kira Loo <youhavebeenkiraed@gmail.com>
2017-08-11 15:02:41 -0700

    add fully functional front end.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,68 @@
+import models.Bouquet;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            ArrayList<Bouquet> allFlowers = Bouquet.getAllFlowers();
+            model.put("allFlowers", allFlowers);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        get("/flowers/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "flower-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/flowers/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String flower1 = request.queryParams("flower1");
+            String flower2 = request.queryParams("flower2");
+            String flower3 = request.queryParams("flower3");
+            String flower4 = request.queryParams("flower4");
+            Bouquet newFlower = new Bouquet(flower1, flower2, flower3, flower4);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/flowers/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfFlowerToFind = Integer.parseInt(req.params("id"));
+            Bouquet flowersMade = Bouquet.findById(idOfFlowerToFind);
+            model.put("flowers", flowersMade);
+            return new ModelAndView(model, "flower-details.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/flowers/:id/update", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfPostToEdit = Integer.parseInt(req.params("id"));
+            Bouquet editFlowers = Bouquet.findById(idOfPostToEdit);
+            model.put("editFlowers", editFlowers);
+            return new ModelAndView(model, "flower-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/flowers/:id/update", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            String flower1 = req.queryParams("flower1");
+            String flower2 = req.queryParams("flower2");
+            String flower3 = req.queryParams("flower3");
+            String flower4 = req.queryParams("flower4");
+            int idOfPostToEdit = Integer.parseInt(req.params("id"));
+            Bouquet editFlowers = Bouquet.findById(idOfPostToEdit);
+            editFlowers.update(flower1, flower2, flower3, flower4);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+    }
 }
--- src/main/java/models/Bouquet.java
+++ src/main/java/models/Bouquet.java
@@ -8,6 +8,7 @@ public class Bouquet {
     private String flower3;
     private String flower4;
 
+    private int id;
     private static ArrayList<Bouquet> allFlowers = new ArrayList<Bouquet>();
 
     public Bouquet(String flower1, String flower2, String flower3, String flower4) {
@@ -16,8 +17,22 @@ public class Bouquet {
         this.flower3 = flower3;
         this.flower4 = flower4;
         allFlowers.add(this);
+        this.id = allFlowers.size();
     }
 
+    public void update(String flower1, String flower2, String flower3, String flower4) {
+        this.flower1 = flower1;
+        this.flower2 = flower2;
+        this.flower3 = flower3;
+        this.flower4 = flower4;
+    }
+
+    public static Bouquet findById(int id){
+        return allFlowers.get(id-1);
+    }
+    public int getId() {
+        return id;
+    }
 
 
 
@@ -30,23 +45,19 @@ public class Bouquet {
         return allFlowers;
     }
 
-    /// getters ///
+    /// getters //////
     public String getFlower1() {
         return flower1;
     }
-
     public String getFlower2() {
         return flower2;
     }
-
     public String getFlower3() {
         return flower3;
     }
-
     public String getFlower4() {
         return flower4;
     }
-
     public static ArrayList<Bouquet> getAllFlowers() {
         return allFlowers;
     }
